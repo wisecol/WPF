8dabf8a6f38915bcebe953f6218b35a4acc39c63
wisecol <seoju047@naver.com>
2023-04-06 09:16:57 +0900

    1st

---
+++ DownLoadingApp/DownLoadingApp/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DownLoadingApp
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application
+    {
+        //Mutex mutex;
+        //
+        ////중복 실행 방지 코드
+        //protected override void OnStartup(StartupEventArgs e)
+        //{
+        //    base.OnStartup(e);
+        //
+        //    string mutexName = "program";
+        //    bool createNew;
+        //
+        //    mutex = new Mutex(true, mutexName, out createNew);
+        //    if (createNew)
+        //    {
+        //        Shutdown();
+        //    }
+        //}
+    }
+}
---
+++ DownLoadingApp/DownLoadingApp/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DownLoadingApp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //progressbar에서 사용
+        BackgroundWorker worker=null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //다운로드 버튼 클릭 할 때
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (worker == null)
+            {
+                worker = new BackgroundWorker();
+                worker.WorkerReportsProgress = true;
+                worker.WorkerSupportsCancellation = true;
+                worker.DoWork += worker_DoWork;
+                worker.ProgressChanged += worker_ProgressChanged;
+                worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+                //호출하여 시작
+                worker.RunWorkerAsync();
+            }            
+        }
+
+        //실행
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        { 
+            for(int i=0; i<100; i++)
+            {
+                worker.ReportProgress(i);
+                Thread.Sleep(100);
+            }
+        }
+
+        //progress 리포트 
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            bar.Value = e.ProgressPercentage;
+        }
+
+        //작업 완료
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            bar.Value = bar.Maximum;
+        }
+
+    }
+}
