3e93d7ea4893544d1884da271fdec907e47fafd7
wisecol <seoju047@naver.com>
2023-04-11 12:27:39 +0900

    reload

---
+++ PopUpApp/PopUpApp/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PopUpApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnPopUpLogout_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        }
+
+        //메뉴 버튼 선택 시 스토리보드 색상 변경
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //UserControl usc = null;
+            GridMain.Children.Clear();
+
+            switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
+            {
+                case "ItmHome":
+                    GridMain.Background=(Brush)Brushes.Tomato;
+                    break;
+                case "ItmCreate":
+                    GridMain.Background = (Brush)Brushes.Violet;
+                    break;
+                case "ItmTickets":
+                    GridMain.Background = (Brush)Brushes.Aqua;
+                    break;
+                case "ItmMessage":
+                    GridMain.Background = (Brush)Brushes.LightGray;
+                    break;
+                case "ItmGitHub":
+                    GridMain.Background = (Brush)Brushes.Gray;
+                    break;
+            }
+        }
+    }
+}
