59baa828c6e8c592e261b79d8f607a0b5ac7e183
wisecol <seoju047@naver.com>
2023-04-12 10:26:15 +0900

    upload20230412

---
+++ DrowDownMenu/DrowDownMenu/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using BeautySolutions.View.ViewModel;
+using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DrowDownMenu
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var menuRegister = new List<SubItem>();
+            menuRegister.Add(new SubItem("Customer", new UserControlCustomers()));
+            menuRegister.Add(new SubItem("Providers", new UserControlProviders()));
+            menuRegister.Add(new SubItem("Employees"));
+            menuRegister.Add(new SubItem("Products"));
+
+            var item6 = new ItemMenu("Register", menuRegister, PackIconKind.Register);
+
+            var menuSchedule = new List<SubItem>();
+            menuSchedule.Add(new SubItem("Services"));
+            menuSchedule.Add(new SubItem("Meetings"));
+            var item1 = new ItemMenu("Appointments", menuSchedule, PackIconKind.Schedule);
+
+            var menuReports = new List<SubItem>();
+            menuReports.Add(new SubItem("Customer"));
+            menuReports.Add(new SubItem("Providers"));            
+            menuReports.Add(new SubItem("Products"));
+            menuReports.Add(new SubItem("Stock"));
+            menuReports.Add(new SubItem("Sales"));
+            var item2 = new ItemMenu("Reports", menuReports, PackIconKind.FileReport);
+
+            var menuExpenses = new List<SubItem>();
+            menuExpenses.Add(new SubItem("Fixed"));
+            menuExpenses.Add(new SubItem("Variable"));
+            var item3 = new ItemMenu("Expenses", menuExpenses, PackIconKind.ShoppingBasket);
+
+            var menuFinancial = new List<SubItem>();
+            menuFinancial.Add(new SubItem("Cash flow"));
+            menuFinancial.Add(new SubItem("Cash cow"));
+            var item4 = new ItemMenu("Financial", menuFinancial, PackIconKind.ScaleBalance);
+
+            Menu.Children.Add(new UserControlMenuItem(item6, this));
+            Menu.Children.Add(new UserControlMenuItem(item1, this));
+            Menu.Children.Add(new UserControlMenuItem(item2, this));
+            Menu.Children.Add(new UserControlMenuItem(item3, this));
+            Menu.Children.Add(new UserControlMenuItem(item4, this));            
+        }
+
+        public void SwitchScreen(object sender)
+        {
+            var screen = (UserControl)sender;
+
+            if (screen != null){
+                StackPanelMain.Children.Clear();
+                StackPanelMain.Children.Add(screen);
+            }
+        }
+
+    }
+}
---
+++ DrowDownMenu/DrowDownMenu/UserControlMenuItem.xaml.cs
@@ -0,0 +1,30 @@
+﻿using BeautySolutions.View.ViewModel;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace DrowDownMenu
+{
+    /// <summary>
+    /// UserControlMenuItem.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class UserControlMenuItem : UserControl
+    {
+        MainWindow _context;
+        public UserControlMenuItem(ItemMenu itemMenu, MainWindow context)
+        {
+            InitializeComponent();
+
+            _context = context;
+
+            ExpanderMenu.Visibility = itemMenu.SubItems == null ? Visibility.Collapsed : Visibility.Visible;
+            ListViewItemMenu.Visibility = itemMenu.SubItems == null ? Visibility.Visible : Visibility.Collapsed;
+
+            this.DataContext = itemMenu;
+        }
+                
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            _context.SwitchScreen(((SubItem)((ListView)sender).SelectedItem).Screen);            
+        }
+    }
+}
