8689ab5df9f016ee6d6fa02bfbc12e3941c7d566
wisecol <seoju047@naver.com>
2023-04-18 16:38:24 +0900

    20230418

---
+++ BikeShop/BikeShop/menu.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BikeShop
+{
+    /// <summary>
+    /// menu.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class menu : Page
+    {
+        public menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Contact.xaml", UriKind.Relative));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Discussion.xaml", UriKind.Relative));
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/ProductsManagement.xaml", UriKind.Relative));
+        }
+    }
+}
