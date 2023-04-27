b7204d403c89d505b4281ccede9fd63696e05c3c
wisecol <seoju047@naver.com>
2023-04-27 12:08:13 +0900

    20230427

---
+++ wpf_test04UT1/wpf_test04UT1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Collections.Generic;
+
+namespace wpf_test04UT1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Consumo consumo = new Consumo();
+            DataContext = new ConsumoViewModel(consumo);
+        }
+        private void ButtonFechar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void GridBarraTitulo_MouseDown(object sender, RoutedEventArgs e)
+        {
+            DragMove();
+        }
+
+
+    }
+    internal class ConsumoViewModel
+    {
+        public List<Consumo> Consumo { get; private set; }
+
+        public ConsumoViewModel(Consumo consumo)
+        {
+            Consumo = new List<Consumo>();
+            Consumo.Add(consumo);
+        }
+    }
+
+    internal class Consumo
+    {
+        public string Titulo { get; private set; }
+        public int Porcentagem { get; private set; }
+        public Consumo()
+        {
+            Titulo = "Consum, Atual";
+            Porcentagem = CalcularPorcentagem();
+
+        }
+        private int CalcularPorcentagem()
+        {
+            return 90; //
+        }
+    }
+}
